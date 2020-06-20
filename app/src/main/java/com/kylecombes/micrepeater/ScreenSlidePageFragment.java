79e4f121c9a62b8273cc9b7339e453c842065e1d
Kelly Yen <kyen@olin.edu>
2020-06-20 14:34:56 -0700

    Created welcome wizard files, just need to finish layout files

---
+++ app/src/main/java/com/kylecombes/micrepeater/ScreenSlidePageFragment.java
@@ -0,0 +1,50 @@
+package com.kylecombes.micrepeater;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class ScreenSlidePageFragment extends Fragment {
+    private static final String ARG_COUNT = "param1";
+    private Integer counter;
+    private int[] layout_files = {
+            R.layout.welcome1, R.layout.welcome2, R.layout.welcome3, R.layout.welcome4, R.layout.welcome5, R.layout.welcome6,
+    };
+
+    public ScreenSlidePageFragment() {
+        // Required empty public constructor
+    }
+    public static ScreenSlidePageFragment newInstance(Integer counter) {
+        ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COUNT, counter);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            counter = getArguments().getInt(ARG_COUNT);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(layout_files[counter], container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
+
