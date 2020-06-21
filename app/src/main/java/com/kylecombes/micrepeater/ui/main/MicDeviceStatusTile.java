dbff206c9ee9d934c5a9a8660fe062f94a025ffa
Kyle Combes <kylecombes@gmail.com>
2020-06-21 18:28:32 -0400

    Rename layout to be more clear.

--- app/src/main/java/com/kylecombes/micrepeater/ui/main/MicDeviceStatusTile.java
+++ app/src/main/java/com/kylecombes/micrepeater/ui/main/MicDeviceStatusTile.java
@@ -14,6 +14,6 @@ public class MicDeviceStatusTile extends ConstraintLayout {
 
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.device_status_tile, this, true);
+        inflater.inflate(R.layout.bluetooth_device_status_tile, this, true);
     }
 }
